a012b182e82de2d89753edc2c4f68947ae64d415
Birdperson9 <yorieshkin@gmail.com>
2023-05-03 11:48:07 +0200

    add Product page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,11 +11,13 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 import Home from './pages/Home'
+import Product from './pages/Product'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<Home />} />
+      <Route path='/product/:id' element={<Product />} />
     </Route>
   )
 )

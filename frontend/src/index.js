69e7b9f8f6ddbdcc06a3390681efa9a0b240386b
Birdperson9 <yorieshkin@gmail.com>
2023-05-03 11:38:44 +0200

    add React Router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,17 +4,29 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './bootstrap.custom.css'
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+import Home from './pages/Home'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<Home />} />
+    </Route>
+  )
+)
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()

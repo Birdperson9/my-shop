0307cd04fe9a5881f5270639ace605080c1c6f84
Birdperson9 <yorieshkin@gmail.com>
2023-05-06 11:11:35 +0200

    add Register page & functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ import Home from './pages/Home'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
 import Login from './pages/Login'
+import Register from './pages/Register'
 import store from './redux/store'
 
 const router = createBrowserRouter(
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
       <Route path='/product/:id' element={<Product />} />
       <Route path='/cart' element={<Cart />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
     </Route>
   )
 )
